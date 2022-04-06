c62cfd4b73ab5bf4dbf788827bf93e88748ddeca
hassanbaryal <h.baryal2@gmail.com>
2022-04-06 18:40:17 -0400

    Completed 09_palidromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'')
+    string = (string.replace(/ /g, '')).toLowerCase()
+    stringA = (Array.from(string)).reverse()
+    stringReverse = stringA.join('')
+    if (string === stringReverse) return true
+    return false
 };
 
 // Do not edit below this line
