9c5069f85fc7b5b091f4361d26c452ab477d5174
hassanbaryal <h.baryal2@gmail.com>
2022-04-06 19:29:44 -0400

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num <= 0) return 'OOPS'
+    let fibSequence = [1]
+    let previousValue = 0
+    while (fibSequence.length < num) {
+        fibSequence.push(previousValue + fibSequence[fibSequence.length - 1])
+        previousValue = fibSequence[fibSequence.length - 2]
+    }
+    
+    return fibSequence[num-1]
 };
 
 // Do not edit below this line
