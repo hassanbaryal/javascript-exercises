fed9e855f2878d4ab1de63a4744d75e00779c8ff
hassanbaryal <h.baryal2@gmail.com>
2022-04-06 16:41:47 -0400

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) return 0
+	const summed = array.reduce((x,y) => {
+    return x + y
+  }, 0)
+  return summed
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length === 0) return 0
+	const multiplied = array.reduce((x,y) => {
+    return x * y
+  }, 1)
+  return multiplied
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y)
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0 || x === 1) return 1
+	let result = x;
+  while (x > 1) {
+    x--
+    result = result * x 
+  }
+  return result
+  // return (x * factorial(x - 1))
 };
 
 // Do not edit below this line
