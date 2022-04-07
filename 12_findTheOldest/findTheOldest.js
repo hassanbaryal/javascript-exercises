fe735b85dd8fbd3c82d373a8c4d69c03f3f04d64
hassanbaryal <h.baryal2@gmail.com>
2022-04-07 15:44:21 -0400

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear()
 
+    const peopleAgeSorted = people.sort((personA, personB) => {
+        if(!(personA.yearOfDeath)) {
+            
+            personA.yearOfDeath = currentYear
+        }
+
+        if(!(personB.yearOfDeath)) {
+            personB.yearOfDeath = currentYear
+        }
+        personAAge = personA.yearOfDeath - personA.yearOfBirth
+        personBAge = personB.yearOfDeath - personB.yearOfBirth
+        return (personAAge - personBAge)
+    })
+    console.log(peopleAgeSorted)
+    return peopleAgeSorted[peopleAgeSorted.length - 1]
 };
 
 // Do not edit below this line
